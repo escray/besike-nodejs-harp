4a7df056b082600622e62d45098c7d4ee5340b55
escray <zescray@gmail.com>
2016-01-27 07:19:38 +0800

    Implement the Mini-Harp module

--- empty.js
+++ empty.js
@@ -1,12 +1,5 @@
 connect = require('connect');
 
-temp =  {};
-
-temp.inside = function(port) {
-    app = connect();
-    console.log("Starting http server on http://localhost:4000");
-    app.listen(port);
-  }
-}
-//var t = temp();
-temp.inside(4000);
+app = connect();
+console.log("Starting http server on http://localhost:4000");
+app.listen(4000);
