a022913080e6625a23658e49c68a553e65ade0bd
escray <zescray@gmail.com>
2016-01-28 11:38:07 +0800

    Reject Stupid Requests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var connect = require('connect');
 var serveServer = require('serve-static');
 var url = require('url');
+var path = require('path');
 var jade = require("./lib/processor/jade.js");
 var less = require("./lib/processor/less.js");
 
@@ -9,9 +10,19 @@ function miniharp(root) {
   var app = connect()
     .use(function(req, res, next){
       if (url.parse(req.url).path === '/') {
+        //res.write("hello: " +  path.basename(req.url));
         req.url += 'index.html';
       }
       next();
+    })
+    .use(function(req, res, next){
+      if ( path.extname(req.url) === '.less' 
+        || path.extname(req.url) === '.jade') {        
+        res.statusCode = 404;
+        res.end('Not Found: ' + req.url);        
+      } else {
+        next();
+      }
     })
     .use('/current-time', function(req, res, next) {
       res.write((new Date()).toISOString());
