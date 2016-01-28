605e59e704422fd780fa23a757a17225c8400255
escray <zescray@gmail.com>
2016-01-28 09:05:20 +0800

    Add less preprocessor to the mini-harp app

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 connect = require('connect');
 serveServer = require('serve-static');
 jade = require("./lib/processor/jade.js");
+less = require("./lib/processor/less.js");
 
 function miniharp(root) {
   //console.log(root);
   var app = connect()
     .use(serveServer(root))
-    .use(jade(root));
+    .use(jade(root))
+    .use(less(root));
   return app;
 };
 
