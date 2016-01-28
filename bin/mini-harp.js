f2213f7498a995b3a7a3a97065e28729cadfb292
escray <zescray@gmail.com>
2016-01-28 10:57:26 +0800

    Serving The Root Path

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,11 +1,13 @@
 #!/usr/local/bin/node
 
 createMiniHarp = require("../")
-console.log(process.cwd());
 var app = createMiniHarp(process.cwd());
 var argv = require('minimist')(process.argv.slice(2));
-
 var port = argv['port'];
+var jade = require('../lib/processor/jade.js');
+
+//console.log(argv['_'][0]);
+var indexjade = argv['_'][0];
 
 var serveStatic = require('serve-static');
 
@@ -18,11 +20,10 @@ if (port == undefined)
 // need judge the port in used ? 
 if (port > 0 && port < 65535) {
   console.log("Starting mini-harp on http://localhost:" + port);
-  app.use('/current-time', function(req, res, next) {
-    res.write((new Date()).toISOString());
-    res.end();
-    next();
-  }).listen(port);  
+
+  app
+  .use(jade(indexjade))   
+  .listen(port);  
 }
 else {
   console.log("port is invalid");
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-connect = require('connect');
-serveServer = require('serve-static');
-jade = require("./lib/processor/jade.js");
-less = require("./lib/processor/less.js");
+var connect = require('connect');
+var serveServer = require('serve-static');
+var url = require('url');
+var jade = require("./lib/processor/jade.js");
+var less = require("./lib/processor/less.js");
 
 function miniharp(root) {
   //console.log(root);
   var app = connect()
+    .use(function(req, res, next){
+      if (url.parse(req.url).path === '/') {
+        req.url += 'index.html';
+      }
+      next();
+    })
+    .use('/current-time', function(req, res, next) {
+      res.write((new Date()).toISOString());
+      res.end();
+      next();
+    })
     .use(serveServer(root))
+
     .use(jade(root))
     .use(less(root));
   return app;
